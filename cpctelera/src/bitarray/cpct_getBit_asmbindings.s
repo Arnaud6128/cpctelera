e67210b65dbafef492a2d9b19064e3c2e4c66eb7
nestornillo <52611892+nestornillo@users.noreply.github.com>
2026-07-13 20:47:17 +0200

    Fix cpct_getBit ASM bindings
    Restored return code from development branch

--- cpctelera/src/bitarray/cpct_getBit_asmbindings.s
+++ cpctelera/src/bitarray/cpct_getBit_asmbindings.s
@@ -17,6 +17,16 @@
 ;;-------------------------------------------------------------------------------
 .module cpct_bitarray
 
+;;
+;; ASM bindings for <cpct_getBit>
+;;
+;;  4/6 microSecs, 3 bytes
+;;
 cpct_getBit_asm::
 
 .include /cpct_getBit.asm/
+ 
+   ;; After testing the target bit, return true or false
+   ret   nz       ;; [2/4] Return A>0 and Flag Z=0 (NZ) when bit was Non-Zero.
+   xor   a        ;; [1] A = 0
+   ret            ;; [3] Return A=0 and Flag Z=1 (Z) otherwise
